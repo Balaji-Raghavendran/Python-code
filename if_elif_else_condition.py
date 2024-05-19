a60548568b3c105edb73474f8266a0b55289c68a
AbhishekRaghavendran <abhishekvj07@gmail.com>
2024-05-19 14:28:40 +0530

    python feature working

---
+++ if_elif_else_condition.py
@@ -0,0 +1,9 @@
+name = input("enter your name:")
+if name == "balaji":
+    print("hi balaji")
+elif name == "abhishek":
+    print("hi abhishek")
+elif name == "nikhil":
+    print("hi nikhil")
+else:
+    print("the person is not there in the list!")
