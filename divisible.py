2b458003bf04e80c540430fbb1a9fa2fbc7ab328
Balaji R <balajiraghavendran09@gmail.com>
2024-06-16 20:56:39 +0530

    Delete divisible.py

--- divisible.py
+++
@@ -1,7 +0,0 @@
-num = int(input("enter a number:"))
-if num % 3 == 0:
-    print("it is divisible by 3")
-elif num % 5 == 0:
-    print("it is divisible by 5")
-else:
-    print("not divisible by 3 and 5")
