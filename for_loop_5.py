a5e7f14371abc1705c14a953a69f0b92135af52a
Balaji R <balajiraghavendran09@gmail.com>
2024-06-16 20:59:00 +0530

    Delete for_loop_5.py

--- for_loop_5.py
+++
@@ -1,9 +0,0 @@
-div_by_3 = 0
-div_by_5 = 0
-for i in range(1,101):
-    if i % 3 == 0:
-        div_by_3 = div_by_3 + 1
-    if i % 5 == 0:
-        div_by_5 = div_by_5 + 1
-print(div_by_3)
-print(div_by_5)
