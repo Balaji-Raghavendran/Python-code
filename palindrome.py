99a3e78bcfd7396b055fffad42949946e5fc0591
Balaji R <balajiraghavendran09@gmail.com>
2024-06-16 21:01:00 +0530

    Delete palindrome.py

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-given_string = input("enter a text:")
-reverse_string = ""
-
-for i in given_string:
-    reverse_string = i + reverse_string
-if reverse_string == given_string:
-    print("the given string is a palindrome")
-else:
-    print("it is not a palindrome")
