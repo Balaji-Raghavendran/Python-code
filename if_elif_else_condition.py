a655f5a328868c98f902234fe49d4a9a3f8945ca
Balaji R <balajiraghavendran09@gmail.com>
2024-06-16 20:59:20 +0530

    Delete if_elif_else_condition.py

--- if_elif_else_condition.py
+++
@@ -1,9 +0,0 @@
-name = input("enter your name:")
-if name == "balaji":
-    print("hi balaji")
-elif name == "abhishek":
-    print("hi abhishek")
-elif name == "nikhil":
-    print("hi nikhil")
-else:
-    print("the person is not there in the list!")
