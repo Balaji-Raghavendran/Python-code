2279432b47b2fb6575208393c01ff7db8680103f
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-09-26 22:44:37 +0530

    hierarchical inheritance oops concept

---
+++ Oops_Concept/hierarchical_inheritance.py
@@ -0,0 +1,15 @@
+class airport:
+    def emiratesflight(self):
+        print("passengers boarding on the flight")
+
+class passenger1(airport):
+    pass
+
+class passenger2(airport):
+    pass
+
+class passenger3(airport):
+    pass
+
+joe=passenger2()
+joe.emiratesflight()
