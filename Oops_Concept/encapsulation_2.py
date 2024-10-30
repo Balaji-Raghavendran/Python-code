913a7abdc75b288eaddcf0a16e34cd034789bef6
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-10-30 16:22:06 +0530

    encapsulation oops concept

---
+++ Oops_Concept/encapsulation_2.py
@@ -0,0 +1,13 @@
+class details:
+    def __init__(self):
+        self.__name="Leo"
+        self.__regno=17
+        self.__dept="Computer Application"
+
+    def school_details(self):
+        print(self.__name)
+        print(self.__regno)
+        print(self.__dept)
+
+student1=details()
+student1.school_details()
