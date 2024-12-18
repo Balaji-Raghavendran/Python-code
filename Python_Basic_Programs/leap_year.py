0749bf536afab107b3a55daacb5280ae06b5c070
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2024-12-18 22:10:21 +0530

    leap year using python

---
+++ Python_Basic_Programs/leap_year.py
@@ -0,0 +1,10 @@
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+year = int(input("Enter a year: "))
+if is_leap_year(year):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
