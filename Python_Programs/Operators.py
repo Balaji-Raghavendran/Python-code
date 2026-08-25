9faf641db879317d390f1abd0212c39ee2e08245
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2026-08-25 21:35:15 +0530

    Add files via upload

---
+++ Python_Programs/Operators.py
@@ -0,0 +1,10 @@
+x = int(input("Enter number 1:"))
+y = int(input("Enter number 2:"))
+
+print(x+y) #addition
+print(x-y) #subtraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x//y) #floor division returns without decimal value
+print(x%y) #modulus reminder value
+print(x**y) #exponent
