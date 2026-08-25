d5681773ac38a1164169d3f054f1913851c29607
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2026-08-25 21:25:17 +0530

    Add files via upload

---
+++ Python_Programs/Input_and_Output.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:") #receives string as input
+age = int(input("Enter your age:")) #receives integer as input
+gpa = float(input("Enter your gpa:")) #receives float as input
+
+print(f"Your name is:{name}")
+print(f"Your age is:{age}")
+print(f"Your gpa is:{gpa}") #formatted string or f-string modern way of printing output
