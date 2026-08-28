121308645643b11156f445007ba218280b82f45d
Balaji Raghavendran <balajiraghavendran09@gmail.com>
2026-08-28 21:05:21 +0530

    Add files via upload

---
+++ Python_Programs/id_checker.py
@@ -0,0 +1,4 @@
+x = 10 # here x is a reference and 10 is an object
+print(id(x)) # id value is 140733885105560
+x = "brand new string" #now x value it has been overrided because it is dynamically typed
+print(id(x)) #id value is 3128572019184
